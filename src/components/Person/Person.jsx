bb18062f50cbf2745d2f79884d0d3dabded1c176
hardaira <hardaira@yahoo.com>
2024-08-24 18:49:37 +0200

    add solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,7 @@
-import cn from 'classnames';
-import './Person.scss';
-
 export const Person = ({ person }) => (
   <div className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
-    <p
-      className={cn('Person__age', {
-        'Person__age--none': person.age === null,
-      })}
-    >
-      I am {person.age}
-    </p>
+    {person.age && <p className="Person__age">I am {person.age}</p>}
     <p className="Person__partner">
       {person.isMarried
         ? `${person.partnerName} is my
