081739c7cad29d7ff5a32a417485b11f1c9e3a10
hardaira <hardaira@yahoo.com>
2024-08-26 12:56:07 +0200

    add solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,6 +1,8 @@
 export const Person = ({ person }) => {
   const marriedMan = person.isMarried && person.sex === 'm';
   const marriedWoman = person.isMarried && person.sex === 'f';
+  
+  return (
   <div className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
     {person.age && <p className="Person__age">I am {person.age}</p>}
@@ -11,5 +13,5 @@ export const Person = ({ person }) => {
       <p className="Person__partner">{person.partnerName} is my husband</p>
     )}
     {!person.isMarried && <p className="Person__partner"> I am not married </p>}
-  </div>;
-};
+  </div>
+)};
