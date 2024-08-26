713fba8c1232042e1ff72d8d314d4630b5833ad8
hardaira <hardaira@yahoo.com>
2024-08-26 12:32:23 +0200

    add solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,15 @@
-export const Person = ({ person }) => (
+export const Person = ({ person }) => {
+  const marriedMan = person.isMarried && person.sex === 'm';
+  const marriedWoman = person.isMarried && person.sex === 'f';
   <div className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
     {person.age && <p className="Person__age">I am {person.age}</p>}
-    {person.sex === 'm'
-          ? <p className="Person__partner"> {person.partnerName} is my wife </p>
-          : <p className="Person__partner"> {person.partnerName} is my husband </p>
-    }
-    {!person.isMarried &&
-      <p className="Person__partner"> I am not married </p>
-    }
-  </div>
-);
+    {marriedMan && (
+      <p className="Person__partner">{person.partnerName} is my wife</p>
+    )}
+    {marriedWoman && (
+      <p className="Person__partner">{person.partnerName} is my husband</p>
+    )}
+    {!person.isMarried && <p className="Person__partner"> I am not married </p>}
+  </div>;
+};
