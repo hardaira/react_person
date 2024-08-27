2a0a827980391e90a881956603b7e7ad9acb63ee
hardaira <hardaira@yahoo.com>
2024-08-27 21:42:31 +0200

    add solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,22 @@
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
-  const marriedMan = isMarried && sex === 'm';
-  const marriedWoman = isMarried && sex === 'f';
+  const { name, age } = person;
+  const getPartnerMessage = partner => {
+    const { isMarried, partnerName, sex } = partner;
+
+    if (!isMarried) {
+      return 'I am not married';
+    }
+
+    return sex === 'f'
+      ? `${partnerName} is my husband`
+      : `${partnerName} is my wife`;
+  };
 
   return (
     <div className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person__age">I am {age}</p>}
-      {marriedMan && (
-        <p className="Person__partner">{partnerName} is my wife</p>
-      )}
-      {marriedWoman && (
-        <p className="Person__partner">{partnerName} is my husband</p>
-      )}
-      {!isMarried && (
-        <p className="Person__partner"> I am not married </p>
-      )}
+      <p className="Person__partner">{getPartnerMessage(person)}</p>
     </div>
   );
 };
